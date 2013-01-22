f0b09e544e4e90cb209ae38b3879cb4f7757aa15
witthaya thatarath <codeengine@live.com>
2013-01-22 21:36:14 +0700

    #694382 - Test modify.

--- branches/development/RankMe/RankMe/Global.asax.cs
+++ branches/development/RankMe/RankMe/Global.asax.cs
@@ -21,7 +21,6 @@ namespace RankMe
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
-			
 			AuthConfig.RegisterAuth();
 
 		}
